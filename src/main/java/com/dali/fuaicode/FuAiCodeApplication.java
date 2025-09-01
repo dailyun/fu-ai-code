863eb0eac7d31c4906441c57bd6ff19a9a2b60c7
86159 <lzc314594272@gmail.com>
2025-09-01 22:05:40 +0800

    初始化

---
+++ src/main/java/com/dali/fuaicode/FuAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.dali.fuaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuAiCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuAiCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dali/fuaicode/FuAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dali.fuaicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuAiCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
