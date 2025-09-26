ed2823f4fc201daa9e92197d0415df9027fe3a18
86159 <lzc314594272@gmail.com>
2025-09-26 13:46:37 +0800

    feat(cache): implement Redis caching for good app pagination - add CacheKeyUtils for key generation and configure RedisCacheManager

---
+++ src/main/java/com/dali/fuaicode/utils/CacheKeyUtils.java
@@ -0,0 +1,15 @@
+package com.dali.fuaicode.utils;
+
+import cn.hutool.crypto.digest.DigestUtil;
+import cn.hutool.json.JSONUtil;
+
+public class CacheKeyUtils {
+
+    public static String generateKey(Object obj) {
+        if (obj == null) {
+            return DigestUtil.md5Hex( "null");
+        }
+        String jsonStr = JSONUtil.toJsonStr( obj);
+        return DigestUtil.md5Hex( jsonStr);
+    }
+}
